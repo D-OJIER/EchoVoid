f97d8b5a78070114d51a6ce0352a2000040fa81f
D-OJIER <reijo23124@gmail.com>
2025-01-26 10:57:25 +0530

    Added Styling to drawer

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -8,6 +8,11 @@ import Chat from '../screens/Chat/Chat';
 import PostsTab from '../components/ProfileTabsComponents/PostsTab';
 import BlogsTab from '../components/ProfileTabsComponents/BlogsTab';
 import Setting from '../screens/Setting/Setting';
+import {StyleSheet, View, Text, Image, TouchableOpacity} from 'react-native';
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -21,13 +26,39 @@ const ProfileTabsNavigation = () => {
     </ProfileTabs.Navigator>
   );
 };
+
+const CustomDrawerContent = props => (
+  <DrawerContentScrollView {...props} style={styles.drawerContainer}>
+    <View style={styles.profileSection}>
+      <Image
+        source={require('../assets/images/343996.jpg')}
+        style={styles.profileImage}
+      />
+      <Text style={styles.profileName}>Voidwalker</Text>
+      <Text style={styles.profileBio}>Explorer of the EchoVoid</Text>
+    </View>
+    <DrawerItemList {...props} />
+  </DrawerContentScrollView>
+);
+
 const MainMenuNavigation = () => {
   return (
     <Drawer.Navigator
+      drawerContent={props => <CustomDrawerContent {...props} />}
       screenOptions={{
-        header: () => null,
         headerShown: false,
-        initialRouteName: Routes.Home,
+        drawerStyle: {
+          backgroundColor: '#572554',
+          width: 280,
+        },
+        drawerLabelStyle: {
+          color: '#D4AF37',
+          fontSize: 16,
+          fontFamily: 'Jersey15-Regular',
+        },
+        drawerActiveBackgroundColor: '#7A3D77',
+        drawerInactiveTintColor: '#D4AF37',
+        drawerActiveTintColor: '#D4AF37',
       }}>
       <Drawer.Screen name={Routes.Home} component={Home} />
       <Drawer.Screen name={Routes.Profile} component={Profile} />
@@ -35,6 +66,7 @@ const MainMenuNavigation = () => {
     </Drawer.Navigator>
   );
 };
+
 const MainNavigation = () => {
   return (
     <Stack.Navigator
@@ -48,5 +80,35 @@ const MainNavigation = () => {
     </Stack.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  drawerContainer: {
+    backgroundColor: '#572554',
+  },
+  profileSection: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#7A3D77',
+    alignItems: 'center',
+  },
+  profileImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    marginBottom: 10,
+    borderWidth: 2,
+    borderColor: '#D4AF37',
+  },
+  profileName: {
+    color: '#D4AF37',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  profileBio: {
+    color: '#997788',
+    fontSize: 14,
+  },
+});
 
 export default MainNavigation;
